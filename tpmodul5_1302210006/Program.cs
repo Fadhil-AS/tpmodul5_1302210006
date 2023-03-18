5a601e52e149dd7dccf76c1b166203439af31267
Muhammad Fadhil Ardiansyah S <mfadhil661@gmail.com>
2023-03-18 13:46:35 +0700

    menambahkan class DataGeneric

--- tpmodul5_1302210006/Program.cs
+++ tpmodul5_1302210006/Program.cs
@@ -1,2 +1,8 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+using tpmodul5_1302210006;
+
+Console.Write("Masukkan NIM: ");
+string nim = Console.ReadLine();
+DataGeneric_1302210006<string> data = new DataGeneric_1302210006<string>(nim);
+data.PrintData_1302210006();
+
